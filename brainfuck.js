8d2b1bb2166ef3507fc9011eb7d9360d6947e805
HUNcamper <selimnahimi@gmail.com>
2016-11-01 00:36:05 +0100

    Fixed loops, added reading

--- brainfuck.js
+++ brainfuck.js
@@ -22,28 +22,52 @@ bot.on('message', message => {
 	var currMessage = message.content.split(" ");
 	
 	if(currMessage[0] == trigger+'run') {
-		var currCode;
 		var con = "";
 		var reply = "";
 		// Concatenate the command for easier running
-		for(var i = 1; i < currMessage.length; i++) {
-			con += currMessage[i];
-		}
-		// Make the currCode into an array of each character
-		currCode = con.split("");
+		var str = trigger+'run ';
+		var currCode = message.content.substring(str.length);
 		// Check each character in the code
 		var cells = [0]; // Cells the interpreter works in
 		var c; // Current character
 		var x = 0; // Current position in the array
-		var y = -1; // (Character) start position of the current loop
+		var y = -1; // (Character) start position of the current loop to jump back to
 		var z = -1; // Cell number of the loop value to read from
 		var error = ""; // If any error occurs, send it here
+		var input = false; var inputOver = false; // is an input currently being read?
+		var iStart = -1; // Start point of the input (in string)
+		var b = 0; // Input length
+		var inputText = [""];
+		var read = 0; // Current position in input text
+		var inputCon = "";
 		for (var i = 0; i < currCode.length; i++) {
 			
 			// THE MAGIC STARTS HERE \\
 			
 			c = currCode[i];
 			switch(c) {
+				case '"':
+					if(!input) {
+						if(!inputOver) {
+							console.log("input start");
+							input = true;
+							iStart = i;
+						} else {
+							error = "Input was already defined";
+							break;
+						}
+					} else {
+						console.log("input end");
+						inputCon = "";
+						for(var l = 0; l < inputText.length; l++) {
+							inputCon += inputText[l];
+						}
+						console.log("input: " + inputCon);
+						input = false;
+						inputOver = true;
+						iStart = -1;
+					}
+					break;
 				case ">":
 					console.log("right");
 					x++;
@@ -80,25 +104,45 @@ bot.on('message', message => {
 						break;
 					}
 					y = i;
-					z = x;
 					break;
 				case "]":
-					console.log("end");
-					if(z == -1) {
+					var end = false;
+					if(x == -1) {
 						error = "Unexpected ']': No loop was defined";
 						break;
 					}
-					if(cells[z] != 0) {
-						i = y;
-					} else {
-						y = -1;
-						z = -1;
+					if(typeof cells[x] !== 'undefined') {
+						if(cells[x] != 0) {
+							i = y;
+							console.log("repeat " + y);
+							break;
+						}
 					}
+					y = -1;
+					z = -1;
+					console.log("end");
+					break;
+				case ",":
+					if(!inputOver) {
+						error = "No input was defined";
+						break;
+					} else if(typeof inputText[read] === 'undefined') {
+						//error = "Input too short (input length: " + b-1 + ", wanted to read: " + read + ")";
+						break;
+					}
+					cells[x] = inputText[read].charCodeAt();
+					console.log("read " + cells[x]);
+					read++;
 					break;
 				case ".":
 					console.log("print");
 					reply += String.fromCharCode(cells[x]);
 					break;
+				default:
+					if(input) {
+						inputText[b] = c;
+						b++;
+					}
 			}
 			
 			// THE MAGIC ENDS HERE
@@ -111,7 +155,10 @@ bot.on('message', message => {
 		} else if(reply == '') reply = 'no output';
 		
 		console.log(reply);
-		message.channel.sendMessage('```'+reply+'```');
+		if(inputCon != "")
+			message.channel.sendMessage('input: **'+inputCon+'**\n'+'```'+reply+'```');
+		else
+			message.channel.sendMessage('```'+reply+'```');
 	} else if(currMessage[0] == trigger+'help') {
 		message.channel.sendMessage('To run a Brainfuck code, type **' + trigger + 'run**!');
 	}
